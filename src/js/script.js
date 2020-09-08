a88d4611e7abcf0b680caeb74edcd27d6ca1203d
amr-asal <errabeka@gmail.com>
2020-09-08 19:03:42 +0200

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
    $('[data-toggle="tooltip"]').tooltip();
-});
+
 $('[data-add-to-cart]').click (function(e){
    alert('اضيف الى عربة الشراء');
    e.stopPropagation();
 });
+$('.product-option input[type="radio"]').change(function(){
+   $(this).parents('.product-option').siblings().removeClass('active');
+   $(this).parents('.product-option').addClass('active');
+})
+});
+
+
